cb99ec0c8f619659e06d9397b23458aecb37bc14
path-yan <ylc109756@gmail.com>
2020-03-16 17:06:56 +0800

    init repo

---
+++ src/main/java/demo/majiang/tcommunity/TcommunityApplication.java
@@ -0,0 +1,14 @@
+package demo.majiang.tcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class TcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/majiang/tcommunity/TcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.majiang.tcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
